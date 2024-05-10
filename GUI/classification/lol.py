2c5bfb1fa96c78c2d5cd3ff201919e560f0fea0a
ali-adel <aliadelsaid2003@gmail.com>
2024-05-10 14:45:22 +0300

    ebst ya3m

---
+++ GUI/classification/lol.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+from tkinter import Button, Entry, Frame, Label, Toplevel
+import tkinter.messagebox
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import load_model
+
+values_neative = {"age": 64,
+          "gender": 1,
+          "impluse": 66,
+          "pressurehight": 160,
+          "pressurelow": 83.0,
+          "glucose": 5.075174,
+          "kcm": 0.587787,
+          "troponin": -4.422849
+          }
+
+
+values_positive = {"age": 21,
+          "gender": 1,
+          "impluse": 94,
+          "pressurehight": 98,
+          "pressurelow": 46,
+          "glucose": 5.690359,
+          "kcm": 1.909543,
+          "troponin": 0.058269
+          }
+
+mymodel = load_model("Advanced-Machine-Learning-\GUI\classification\ANN.h5")
+
+# Convert values dictionary to DataFrame
+input_df = pd.DataFrame(values_neative, index=[0])
+
+scaler = StandardScaler()
+
+def scaling(df):
+    scaled_df = scaler.fit_transform(df)
+    return scaled_df
+
+# Log transformation
+# Log transformation
+def log_transform(df, col_name):
+    col_transformed = np.log(df[col_name] + 1e-10)  # Adding a small constant to prevent zero or negative values
+    return col_transformed
+
+# Apply log transformation to each column
+
+input_df['glucose'] = log_transform(input_df, "glucose")
+input_df['kcm'] = log_transform(input_df, "kcm")
+input_df['troponin'] = log_transform(input_df, "troponin")
+
+# Scale the transformed data
+sc_df = scaling(input_df)
+
+# Now you can use the input_list for predictions with your model
+predictions = mymodel.predict(sc_df)
+
+threshold = 0.5
+y_pred_ann = mymodel.predict(sc_df)
+y_pred_binary = np.where(y_pred_ann >= threshold, 1, 0)
+
+print("Predictions:", predictions)
+print("Predictions:", y_pred_binary)
+if y_pred_binary[0] == 0:
+     print("The patient does not have heart problems")
+else:
+     print("The patient has heart problems")
